523c190c7af4688be7d883d363850a84b5450110
Taqi Tahmid <mdtaqitahmid@gmail.com>
2025-07-02 18:40:54 +0300

    kubernetes: move jellyfin to private ingress

--- infra/terraform/kubernetes/backend.tf
+++ infra/terraform/kubernetes/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket                      = "terraform-state" # Name of the MinIO bucket
-    key                         = "proxmox/terraform.tfstate" # Path to the state file in the bucket
+    key                         = "kubernetes/terraform.tfstate" # Path to the state file in the bucket
     endpoint                    = var.minio_endpoint # MinIO API endpoint
     access_key                  = var.minio_access_key # MinIO access key
     secret_key                  = var.minio_secret_key # MinIO secret key
